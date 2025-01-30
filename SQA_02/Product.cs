ae46b0cad47709acc0db4aae80124bb28cb535cc
Mauricio Lechuga <144956516+mauriciolechugaCA@users.noreply.github.com>
2025-01-30 12:47:11 -0500

    Add project files.

---
+++ SQA_02/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace SQA_02
+{
+    public class Product
+    {
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public double ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        public Product(int prodID, string prodName, double itemPrice, int stockAmount)
+        {
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        public int IncreaseStock(int amount)
+        {
+            StockAmount += amount;
+            return StockAmount;
+        }
+
+        public int DecreaseStock(int amount)
+        {
+            StockAmount -= amount;
+            return StockAmount;
+        }
+    }
+}
---
+++ SQA_A02/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQA_A02
+{
+    internal class Product
+    {
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public double ItemPrice { get; set; }
+        public int ItemStock { get; set; }
+    }
+}
