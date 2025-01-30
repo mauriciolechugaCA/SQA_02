255113748d134616d6e73df9d96a9bd1f301a9f0
Mauricio Lechuga <144956516+mauriciolechugaCA@users.noreply.github.com>
2025-01-30 13:42:56 -0500

    Added summaries and placeholders for tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,10 +7,150 @@ namespace TestProject1
         {
         }
 
+        /// <summary>
+        /// Test cases for ProductID
+        /// </summary>
         [Test]
+        // ProdID Test 1
         public void Test1()
         {
             Assert.Pass();
         }
+
+        [Test]
+        // ProdID Test 2
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // ProdID Test 3
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Test cases for ProductName
+        /// </summary>
+        [Test]
+        // ProdName Test 1
+        public void Test11()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // ProdName Test 2
+        public void Test12()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // ProdName Test 3
+        public void Test13()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Test cases for ItemPrice
+        /// </summary>
+        [Test]
+        // ItemPrice Test 1
+        public void Test111()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // ItemPrice Test 2
+        public void Test112()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // ItemPrice Test 3
+        public void Test113()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Test cases for StockAmount
+        /// </summary>
+        [Test]
+        // StockAmount Test 1
+        public void Test1111()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // StockAmount Test 2
+        public void Test1112()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // StockAmount Test 3
+        public void Test1113()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Test cases for IncreaseStock
+        /// </summary>
+        [Test]
+        // IncreaseStock Test 
+        public void Test11111()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // IncreaseStock Test 2
+        public void Test11112()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // IncreaseStock Test 3
+        public void Test11113()
+        {
+            Assert.Pass();
+        }
+
+        /// <summary>
+        /// Test cases for DecreaseStock
+        /// </summary>
+
+        [Test]
+        // DecreaseStock Test 1
+        public void Test111111()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // DecreaseStock Test 2
+        public void Test111112()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        // DecreaseStock Test 3
+        public void Test111113()
+        {
+            Assert.Pass();
+        }
+
     }
 }
